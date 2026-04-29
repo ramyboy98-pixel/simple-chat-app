42b1cefebd7cde1e218521f341e99a1213c5ca98
ramyboy98-pixel <ramyboy98@gmail.com>
2026-04-29 22:27:08 +0100

    Update Firebase configuration and code formatting

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
 import {
 getDatabase,
 ref,
@@ -9,37 +10,29 @@ from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
 
 
 const firebaseConfig = {
-
-apiKey: "ضع apiKey من Firebase",
-
-authDomain: "ضع authDomain",
-
-databaseURL: "ضع databaseURL",
-
-projectId: "ضع projectId",
-
-storageBucket: "ضع storageBucket",
-
-messagingSenderId: "ضع messagingSenderId",
-
-appId: "ضع appId"
-
+apiKey: "AIzaSyAFPjYx14j-hmgHwj0idAifGCg0pJdZK7s",
+authDomain: "simple-chat-app-90b82.firebaseapp.com",
+databaseURL: "https://simple-chat-app-90b82-default-rtdb.europe-west1.firebasedatabase.app",
+projectId: "simple-chat-app-90b82",
+storageBucket: "simple-chat-app-90b82.firebasestorage.app",
+messagingSenderId: "569901832745",
+appId: "1:569901832745:web:b20e023d9d0eb6fdb01a38"
 };
 
 
 const app = initializeApp(firebaseConfig);
 
-const db=getDatabase(app);
+const db = getDatabase(app);
 
-const messagesRef=ref(db,"messages");
+const messagesRef = ref(db,"messages");
 
 
-window.sendMessage=function(){
+window.sendMessage = function(){
 
-let name=
+let name =
 document.getElementById("nameInput").value;
 
-let text=
+let text =
 document.getElementById("messageInput").value;
 
 if(!name || !text) return;
@@ -50,9 +43,7 @@ text:text,
 time:Date.now()
 });
 
-document.getElementById(
-"messageInput"
-).value="";
+document.getElementById("messageInput").value="";
 
 };
 
